39189457a4c15d5cc2bf6d497a17ef35236260f3
czy <1169197532@qq.com>
2019-04-25 14:50:23 +0800

    商家管理后台完成2

--- pinyougou-web/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou-web/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -4,6 +4,64 @@ app.controller('cartController', function ($scope, $controller, baseService) {
     // 继承baseController
     $controller('baseController', {$scope : $scope});
 
+    // 定义提交购物车订单集合
+    $scope.orderCarts=[];
+
+    //往购物车订单里加订单
+    $scope.addOrderCart=function (x,y,event) {
+        var cart = $scope.isExistSeller($scope.orderCarts,$scope.carts[x]);
+        if (cart){
+            if (event.target.checked){
+                cart.orderItems.push($scope.carts[x].orderItems[y]);
+            }else {
+                var index = cart.orderItems.indexOf($scope.carts[x].orderItems[y]);
+                cart.orderItems.splice(index,1);
+                if (cart.orderItems.length<=0){
+                    var i = $scope.orderCarts.indexOf(cart);
+                    $scope.orderCarts.splice(i,1);
+                }
+            }
+        } else {
+            cart=JSON.parse(JSON.stringify($scope.carts[x]));
+            cart.orderItems=[];
+            cart.orderItems.push($scope.carts[x].orderItems[y]);
+            $scope.orderCarts.push(cart);
+        }
+    };
+
+    //商家全选
+    $scope.sellerSelect=function (x,event) {
+        var ev = {target:{checked:false}};
+        if(event.target.checked){
+            ev.target.checked=true;
+            var cart = $scope.isExistSeller($scope.orderCarts,$scope.carts[x])
+            if (cart){
+                var ind = $scope.orderCarts.indexOf(cart);
+                $scope.orderCarts.splice(ind,1);
+            }
+        }
+        for(var i = 0;i < $scope.carts[x].orderItems.length;i++){
+            $scope.addOrderCart(x,i,ev);
+        }
+        $.each($("input[name="+x+"]"),function (k,v) {
+            if(event.target.checked){
+                $(v).prop("checked",true);
+            }else {
+                $(v).prop("checked",false);
+            }
+        });
+    };
+
+    //判断是够已存在此商家
+    $scope.isExistSeller=function (carts,cart) {
+        for(var i = 0;i < carts.length;i++){
+            if (carts[i].sellerName == cart.sellerName){
+                return carts[i];
+            }
+        }
+        return null;
+    };
+
     $scope.findCartList=function () {
         baseService.sendGet("/cart/findCart").then(function (response) {
             $scope.carts = response.data;
